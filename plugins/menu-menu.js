5fedb04ee391be2da6317505d4d795280719d72e
Aloush775 <112141479+aliahmadhassanhassaun@users.noreply.github.com>
2023-02-28 20:48:54 +0300

    Update menu-menu.js

--- plugins/menu-menu.js
+++ plugins/menu-menu.js
@@ -1,19 +1,19 @@
-import fs from 'fs'
-import fetch from 'node-fetch'
-import { xpRange } from '../lib/levelling.js'
-const { levelling } = '../lib/levelling.js'
-import PhoneNumber from 'awesome-phonenumber'
-import { promises } from 'fs'
-import { join } from 'path'
+import fs from  fs 
+import fetch from  node-fetch 
+import { xpRange } from  ../lib/levelling.js 
+const { levelling } =  ../lib/levelling.js 
+import PhoneNumber from  awesome-phonenumber 
+import { promises } from  fs 
+import { join } from  path 
 let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) => {
 try {
-let vn = './media/menu.mp3'
+let vn =  ./media/menu.mp3 
 let pp = imagen4
-let img = await(await fetch('https://www.paidmembershipspro.com/wp-content/uploads/2017/07/PayPal-Express.png')).buffer()
+let img = await(await fetch( https://www.paidmembershipspro.com/wp-content/uploads/2017/07/PayPal-Express.png )).buffer()
 let d = new Date(new Date + 3600000)
-let locale = 'es'
-let week = d.toLocaleDateString(locale, { weekday: 'long' })
-let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+let locale =  es 
+let week = d.toLocaleDateString(locale, { weekday:  long  })
+let date = d.toLocaleDateString(locale, { day:  numeric , month:  long , year:  numeric  })
 let _uptime = process.uptime() * 1000
 let uptime = clockString(_uptime)
 let user = global.db.data.users[m.sender]
@@ -23,7 +23,7 @@ let { min, xp, max } = xpRange(level, global.multiplier)
 let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
 let more = String.fromCharCode(8206)
 let readMore = more.repeat(850)   
-let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let taguser =  @  + m.sender.split("@s.whatsapp.net")[0]
 let str = `
 ║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
 *شبيك لبيك بوت القياده الخال بين يديك🦅🇾🇪*
@@ -32,7 +32,7 @@ let str = `
 t.me/TheYemenigentle
 ║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
 ╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
-║    ◉— *بوت القياده الخال شخصياً* —◉
+║    ◉— *𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭* —◉
 ║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
 ║➤ *𝗛ola, @967714427587*
 ║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
@@ -389,9 +389,9 @@ t.me/Driving_uncle_personally/5205
 ┗━━━━━━━━━━━━━━━━┛
 `.trim()
 let buttons = [
-{ buttonId: '#donar', buttonText: { displayText: '📮 المالك 📮' }, type: 1 },
-//{ buttonId: '#terminosycondiciones', buttonText: { displayText: '📋 𝚃𝙴𝚁𝙼𝙸𝙽𝙾𝚂 𝚈 𝙲𝙾𝙽𝙳𝙸𝙲𝙸𝙾𝙽𝙴𝚂 📋' }, type: 1 }]
-{ buttonId: '#infobot', buttonText: { displayText: '🐾 المطور 🐾' }, type: 1 }]
+{ buttonId:  #donar , buttonText: { displayText:  📮 المطور 📮  }, type: 1 },
+//{ buttonId:  #terminosycondiciones , buttonText: { displayText:  📋 اختيار المزيد   📋  }, type: 1 }]
+{ buttonId:  #infobot , buttonText: { displayText:  🐾 𝙸𝙽𝙵𝙾𝙱𝙾𝚃 🐾  }, type: 1 }]
 let buttonMessage = {
 image: pp,
 caption: str.trim(),
@@ -403,24 +403,24 @@ contextInfo: {
 mentionedJid: [m.sender],
 externalAdReply: {
 showAdAttribution: true,
-mediaType: 'VIDEO',
+mediaType:  VIDEO ,
 mediaUrl: null,
-title: '👑 بوت القياده الخال 👑',
+title:  👑 بوت القياده الخال شخصياً 👑 ,
 body: null,
 thumbnail: img,
 sourceUrl: `https://www.paypal.me/TheShadowBrokers133`
 }}}
 conn.sendMessage(m.chat, buttonMessage, { quoted: m })
-//await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+//await conn.sendFile(m.chat, vn,  menu.mp3 , null, m, true, { type:  audioMessage , ptt: true})
 } catch {
-conn.reply(m.chat, '*[❗خطاء❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m)
+conn.reply(m.chat,  *[❗خطاء❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃* , m)
 }}
 handler.command = /^(menu|menú|memu|memú|help|info|comandos|allmenu|2help|menu1.2|ayuda|commands|commandos|cmd)$/i
 handler.exp = 50
 handler.fail = null
 export default handler
 function clockString(ms) {
-let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
+let h = isNaN(ms) ?  --  : Math.floor(ms / 3600000)
+let m = isNaN(ms) ?  --  : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ?  --  : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join( : )}
