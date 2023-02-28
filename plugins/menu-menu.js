aee49a972c415e5c0ca4b2296f8620167916bd7e
Aloush775 <112141479+aliahmadhassanhassaun@users.noreply.github.com>
2023-02-28 20:20:31 +0300

    Add files via upload

---
+++ plugins/menu-menu.js
@@ -0,0 +1,426 @@
+import fs from 'fs'
+import fetch from 'node-fetch'
+import { xpRange } from '../lib/levelling.js'
+const { levelling } = '../lib/levelling.js'
+import PhoneNumber from 'awesome-phonenumber'
+import { promises } from 'fs'
+import { join } from 'path'
+let handler = async (m, { conn, usedPrefix, usedPrefix: _p, __dirname, text }) => {
+try {
+let vn = './media/menu.mp3'
+let pp = imagen4
+let img = await(await fetch('https://www.paidmembershipspro.com/wp-content/uploads/2017/07/PayPal-Express.png')).buffer()
+let d = new Date(new Date + 3600000)
+let locale = 'es'
+let week = d.toLocaleDateString(locale, { weekday: 'long' })
+let date = d.toLocaleDateString(locale, { day: 'numeric', month: 'long', year: 'numeric' })
+let _uptime = process.uptime() * 1000
+let uptime = clockString(_uptime)
+let user = global.db.data.users[m.sender]
+let { money, joincount } = global.db.data.users[m.sender]
+let { exp, limit, level, role } = global.db.data.users[m.sender]
+let { min, xp, max } = xpRange(level, global.multiplier)
+let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length 
+let more = String.fromCharCode(8206)
+let readMore = more.repeat(850)   
+let taguser = '@' + m.sender.split("@s.whatsapp.net")[0]
+let str = `
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
+*شبيك لبيك بوت القياده الخال بين يديك🦅🇾🇪*
+*مطور البوت القياده الخال شخصياً* 
+*اذا توقف معك البوت تواصل معي تليجرام*
+t.me/TheYemenigentle
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
+╭═══〘 ✯✯✯✯✯✯✯✯✯ 〙══╮
+║    ◉— *بوت القياده الخال شخصياً* —◉
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
+║➤ *𝗛ola, @967714427587*
+║≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡║
+║➤ *Owner:* Bruno Sobrino
+║➤ *معرفي تليجرام:* http://t.me/TheYemenigentle
+║➤ *قناتي تليجرام:* t.me/Driving_uncle_personally
+║➤ *رقمي واتساب:* https://wa.me/+967711023486
+║ *القياده الخال شخصياً 🦅🇾🇪*
+╰═══╡✯✯✯✯✯✯✯✯✯╞═══╯
+┗━━━━━━━━━━━━━━━━┛  
+*مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< اوامر الالعاب في البوت 👇🏼 />*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 🎖️ _.mates *<noob / easy / medium / hard / extreme /impossible /impossible2>*_
+┣ ඬ⃟ 🎖️ _.ppt *<papel / tijera /piedra>*_
+┣ ඬ⃟ 🎖️ _.prostituto *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.prostituta *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.gay2 *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.lesbiana *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.pajero *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.pajera *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.puto *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.puta *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.manco *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.manca *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.rata *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.love *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.doxear *<nombre / @tag>*_
+┣ ඬ⃟ 🎖️ _.pregunta *<texto>*_
+┣ ඬ⃟ 🎖️ _.suitpvp *<@tag>*_
+┣ ඬ⃟ 🎖️ _.slot *<apuesta>*_
+┣ ඬ⃟ 🎖️ _.ttt *<nombre sala>*_
+┣ ඬ⃟ 🎖️ _.delttt_
+┣ ඬ⃟ 🎖️ _.acertijo_
+┣ ඬ⃟ 🎖️ _.simi *<texto>*_
+┣ ඬ⃟ 🎖️ _.top *<texto>*_
+┣ ඬ⃟ 🎖️ _.topgays_
+┣ ඬ⃟ 🎖️ _.topotakus_
+┣ ඬ⃟ 🎖️ _.formarpareja_
+┣ ඬ⃟ 🎖️ _.verdad_
+┣ ඬ⃟ 🎖️ _.reto_
+┣ ඬ⃟ 🎖️ _.cancion_
+┣ ඬ⃟ 🎖️ _.pista_
+┣ ඬ⃟ 🎖️ _.akinator_
+┏━━━━━━━━━━━━━━━━┓
+┃ *< اوامر تنزيل الفديو من تيك توك او فيس او انستا او يوتيوب عبر رابط فقط 👇🏼 />*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 📥 _.instagram *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.instagram2 *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.instagram3 *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.mediafire *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.instagram *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.gitclone *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.gdrive *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.tiktok *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.xnxxdl *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.xvideosdl *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.twitter *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.fb *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.fb2 *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.fb3 *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.fb4 *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.fb5 *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.ytmp3 *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.ytmp4 *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.ytmp3doc *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.ytmp4doc *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.dapk2 *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.stickerpack *<enlace / link / url>*_
+┣ ඬ⃟ 📥 _.play *<texto>*_
+┣ ඬ⃟ 📥 _.play.1 *<texto>*_
+┣ ඬ⃟ 📥 _.play.2 *<texto>*_
+┣ ඬ⃟ 📥 _.playdoc *<texto>*_
+┣ ඬ⃟ 📥 _.playlist *<texto>*_
+┣ ඬ⃟ 📥 _.playlist2 *<texto>*_
+┣ ඬ⃟ 📥 _.spotify *<texto>*_
+┣ ඬ⃟ 📥 _.ringtone *<texto>*_
+┣ ඬ⃟ 📥 _.soundcloud *<texto>*_
+┣ ඬ⃟ 📥 _.imagen *<texto>*_
+┣ ඬ⃟ 📥 _.pinterest *<texto>*_
+┣ ඬ⃟ 📥 _.wallpaper *<texto>*_
+┣ ඬ⃟ 📥 _.wallpaper2 *<texto>*_
+┣ ඬ⃟ 📥 _.pptiktok *<nombre de usuario>*_
+┣ ඬ⃟ 📥 _.igstalk *<nombre de usuario>*_
+┣ ඬ⃟ 📥 _.igstory *<nombre de usuario>*_
+┣ ඬ⃟ 📥 _.tiktokstalk *<username>*_
+┗━━━━━━━━━━━━━━━━┛
+ *مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< اوامر الجروبات🦸🏽‍♂️ />* 
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 💎 _.add *<numero>*_
+┣ ඬ⃟ 💎 _.kick *<@tag>*_
+┣ ඬ⃟ 💎 _.kick2 *<@tag>*_
+┣ ඬ⃟ 💎 _.listanum *<texto>*_
+┣ ඬ⃟ 💎 _.kicknum *<texto>*_
+┣ ඬ⃟ 💎 _.grupo *<abrir / cerrar>*_
+┣ ඬ⃟ 💎 _.grouptime *<opcion> <tiempo>*_
+┣ ඬ⃟ 💎 _.promote *<@tag>*_
+┣ ඬ⃟ 💎 _.demote *<@tag>*_
+┣ ඬ⃟ 💎 _admins *<texto>*_ (𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)
+┣ ඬ⃟ 💎 _.demote *<@tag>*_
+┣ ඬ⃟ 💎 _.infogroup_
+┣ ඬ⃟ 💎 _.resetlink_
+┣ ඬ⃟ 💎 _.link_
+┣ ඬ⃟ 💎 _.setname *<texto>*_
+┣ ඬ⃟ 💎 _.setdesc *<texto>*_
+┣ ඬ⃟ 💎 _.invocar *<texto>*_
+┣ ඬ⃟ 💎 _.setwelcome *<texto>*_
+┣ ඬ⃟ 💎 _.setbye *<texto>*_
+┣ ඬ⃟ 💎 _.hidetag *<texto>*_
+┣ ඬ⃟ 💎 _.hidetag *<audio>*_
+┣ ඬ⃟ 💎 _.hidetag *<video>*_
+┣ ඬ⃟ 💎 _.hidetag *<imagen>*_
+┣ ඬ⃟ 💎 _.warn *<@tag>*_
+┣ ඬ⃟ 💎 _.unwarn *<@tag>*_
+┣ ඬ⃟ 💎 _.listwarn_
+┣ ඬ⃟ 💎 _.fantasmas_
+┣ ඬ⃟ 💎 _.destraba_
+┣ ඬ⃟ 💎 _.setpp *<imagen>*_
+┗━━━━━━━━━━━━━━━━┛
+ *مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< اوامر تحويل صوت واتساب الى mp3 />*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 🧧 _.toanime *<imagen>*_
+┣ ඬ⃟ 🧧 _.togifaud *<video>*_
+┣ ඬ⃟ 🧧 _.toimg *<sticker>*_
+┣ ඬ⃟ 🧧 _.tomp3 *<video>*_
+┣ ඬ⃟ 🧧 _.tomp3 *<nota de voz>*_
+┣ ඬ⃟ 🧧 _.toptt *<video / audio>*_
+┣ ඬ⃟ 🧧 _.tovideo *<sticker>*_
+┣ ඬ⃟ 🧧 _.tourl *<video / imagen / audio>*_
+┣ ඬ⃟ 🧧 _.tts es *<texto>*_
+┗━━━━━━━━━━━━━━━━┛
+ *مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< اوامر تصميم اسمك الى صوره 👇🏼 👇🏼 />*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 🖍️ _.phmaker *<opcion> <imagen>*_
+┣ ඬ⃟ 🖍️ _.logos *<efecto> <texto>*_
+┣ ඬ⃟ 🖍️ _.logochristmas *<texto>*_
+┣ ඬ⃟ 🖍️ _.logocorazon *<texto>*_
+┣ ඬ⃟ 🖍️ _.ytcomment *<texto>*_
+┣ ඬ⃟ 🖍️ _.hornycard *<@tag>*_
+┣ ඬ⃟ 🖍️ _.simpcard *<@tag>*_
+┣ ඬ⃟ 🖍️ _.lolice *<@tag>*_
+┣ ඬ⃟ 🖍️ _.itssostupid_
+┣ ඬ⃟ 🖍️ _.pixelar_
+┣ ඬ⃟ 🖍️ _.blur_
+┗━━━━━━━━━━━━━━━━┛
+ *مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< اوامر البحث عن صور انمي 👇🏼 />*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 👾 _.kpop *<blackpink / exo / bts>*_
+┣ ඬ⃟ 👾 _.cristianoronaldo_
+┣ ඬ⃟ 👾 _.messi_
+┣ ඬ⃟ 👾 _.meme_
+┣ ඬ⃟ 👾 _.itzy_
+┣ ඬ⃟ 👾 _.blackpink_
+┣ ඬ⃟ 👾 _.lolivid_
+┣ ඬ⃟ 👾 _.loli_
+┣ ඬ⃟ 👾 _.navidad_
+┣ ඬ⃟ 👾 _.ppcouple_
+┣ ඬ⃟ 👾 _.wpmontaña_
+┣ ඬ⃟ 👾 _.pubg_
+┣ ඬ⃟ 👾 _.wpgaming_
+┣ ඬ⃟ 👾 _.wpaesthetic_
+┣ ඬ⃟ 👾 _.wpaesthetic2_
+┣ ඬ⃟ 👾 _.wprandom_
+┣ ඬ⃟ 👾 _.wallhp_
+┣ ඬ⃟ 👾 _.wpvehiculo_
+┣ ඬ⃟ 👾 _.wpmoto_
+┣ ඬ⃟ 👾 _.coffee_
+┣ ඬ⃟ 👾 _.pentol_
+┣ ඬ⃟ 👾 _.caricatura_
+┣ ඬ⃟ 👾 _.ciberespacio_
+┣ ඬ⃟ 👾 _.technology_
+┣ ඬ⃟ 👾 _.doraemon_
+┣ ඬ⃟ 👾 _.hacker_
+┣ ඬ⃟ 👾 _.planeta_
+┣ ඬ⃟ 👾 _.randomprofile_
+┣ ඬ⃟ 👾 _.neko_
+┣ ඬ⃟ 👾 _.waifu_
+┣ ඬ⃟ 👾 _.akira_
+┣ ඬ⃟ 👾 _.akiyama_
+┣ ඬ⃟ 👾 _.anna_
+┣ ඬ⃟ 👾 _.asuna_
+┣ ඬ⃟ 👾 _.ayuzawa_
+┣ ඬ⃟ 👾 _.boruto_
+┣ ඬ⃟ 👾 _.chiho_
+┣ ඬ⃟ 👾 _.chitoge_
+┣ ඬ⃟ 👾 _.deidara_
+┣ ඬ⃟ 👾 _.erza_
+┣ ඬ⃟ 👾 _.elaina_
+┣ ඬ⃟ 👾 _.eba_
+┣ ඬ⃟ 👾 _.emilia_
+┣ ඬ⃟ 👾 _.hestia_
+┣ ඬ⃟ 👾 _.hinata_
+┣ ඬ⃟ 👾 _.inori_
+┣ ඬ⃟ 👾 _.isuzu_
+┣ ඬ⃟ 👾 _.itachi_
+┣ ඬ⃟ 👾 _.itori_
+┣ ඬ⃟ 👾 _.kaga_
+┗━━━━━━━━━━━━━━━━┛
+ *مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< اوامر بدء البوت />*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 📳 _.start_
+┣ ඬ⃟ 📳 _.next_
+┣ ඬ⃟ 📳 _.leave_
+┗━━━━━━━━━━━━━━━━┛
+ *مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< اوامر البحث في السوشيال ميديا👇🏼 />*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 🔍 _.cuevana *<texto>*_
+┣ ඬ⃟ 🔍 _.pelisplus *<texto>*_
+┣ ඬ⃟ 🔍 _.modapk *<texto>*_
+┣ ඬ⃟ 🔍 _.stickersearch *<texto>*_
+┣ ඬ⃟ 🔍 _.stickersearch2 *<texto>*_
+┣ ඬ⃟ 🔍 _.xnxxsearch *<texto>*_
+┣ ඬ⃟ 🔍 _.animeinfo *<texto>*_
+┣ ඬ⃟ 🔍 _.google *<texto>*_
+┣ ඬ⃟ 🔍 _.letra *<texto>*_
+┣ ඬ⃟ 🔍 _.wikipedia *<texto>*_
+┣ ඬ⃟ 🔍 _.ytsearch *<texto>*_
+┣ ඬ⃟ 🔍 _.playstore *<texto>*_
+┗━━━━━━━━━━━━━━━━┛
+ *مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< بعض من قوائم البحث عن الصوت  />*   
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┃ *- 𝙴𝚂𝙲𝚁𝙸𝙱𝙴 𝙻𝙰𝚂 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴𝚂 𝙿𝙰𝙻𝙰𝙱𝚁𝙰𝚂 𝙾 𝙵𝚁𝙰𝚂𝙴𝚂 𝚂𝙸𝙽 𝙽𝙸𝙽𝙶𝚄𝙽 𝙿𝚁𝙴𝙵𝙸𝙹𝙾 (#, /, *, .)* 
+┃ _- (𝑢𝑠𝑜 𝑠𝑖𝑛 𝑝𝑟𝑒𝑓𝑖𝑗𝑜)_
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 🔊 _Quien es tu sempai botsito 7w7_
+┣ ඬ⃟ 🔊 _Te diagnostico con gay_
+┣ ඬ⃟ 🔊 _A nadie le importa_
+┣ ඬ⃟ 🔊 _Fiesta del admin_
+┣ ඬ⃟ 🔊 _Fiesta del administrador_ 
+┣ ඬ⃟ 🔊 _Vivan los novios_
+┣ ඬ⃟ 🔊 _Feliz cumpleaños_
+┣ ඬ⃟ 🔊 _Noche de paz_
+┣ ඬ⃟ 🔊 _Buenos dias_
+┣ ඬ⃟ 🔊 _Buenos tardes_
+┣ ඬ⃟ 🔊 _Buenos noches_
+┣ ඬ⃟ 🔊 _Audio hentai_
+┣ ඬ⃟ 🔊 _Chica lgante_
+┣ ඬ⃟ 🔊 _Feliz navidad_
+┣ ඬ⃟ 🔊 _Vete a la vrg_
+┣ ඬ⃟ 🔊 _Pasa pack Bot_
+┣ ඬ⃟ 🔊 _Atencion grupo_
+┣ ඬ⃟ 🔊 _Marica quien_
+┣ ඬ⃟ 🔊 _Murio el grupo_
+┣ ඬ⃟ 🔊 _Oh me vengo_
+┣ ඬ⃟ 🔊 _tio que rico_
+┣ ඬ⃟ 🔊 _Viernes_
+┣ ඬ⃟ 🔊 _Baneado_
+┣ ඬ⃟ 🔊 _Sexo_
+┣ ඬ⃟ 🔊 _Hola_
+┣ ඬ⃟ 🔊 _Un pato_
+┣ ඬ⃟ 🔊 _Nyanpasu_
+┣ ඬ⃟ 🔊 _Te amo_
+┣ ඬ⃟ 🔊 _Yamete_
+┣ ඬ⃟ 🔊 _Bañate_
+┣ ඬ⃟ 🔊 _Es puto_
+┣ ඬ⃟ 🔊 _La biblia_
+┣ ඬ⃟ 🔊 _Onichan_
+┣ ඬ⃟ 🔊 _Mierda de Bot_
+┣ ඬ⃟ 🔊 _Siuuu_
+┣ ඬ⃟ 🔊 _Epico_
+┣ ඬ⃟ 🔊 _Shitpost_
+┣ ඬ⃟ 🔊 _Rawr_
+┣ ඬ⃟ 🔊 _UwU_
+┣ ඬ⃟ 🔊 _:c_
+┣ ඬ⃟ 🔊 _a_
+┗━━━━━━━━━━━━━━━━┛
+ *مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< بعض من قوائم الالعاب 👇🏼 />*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 💵 _.adventure_
+┣ ඬ⃟ 💵 _.cazar_
+┣ ඬ⃟ 💵 _.cofre_
+┣ ඬ⃟ 💵 _.balance_
+┣ ඬ⃟ 💵 _.claim_
+┣ ඬ⃟ 💵 _.heal_
+┣ ඬ⃟ 💵 _.lb_
+┣ ඬ⃟ 💵 _.levelup_
+┣ ඬ⃟ 💵 _.myns_
+┣ ඬ⃟ 💵 _.perfil_
+┣ ඬ⃟ 💵 _.work_
+┣ ඬ⃟ 💵 _.minar_
+┣ ඬ⃟ 💵 _.minar2_
+┣ ඬ⃟ 💵 _.buy_
+┣ ඬ⃟ 💵 _.buyall_
+┣ ඬ⃟ 💵 _.verificar_
+┣ ඬ⃟ 💵 _.robar *<cantidad> <@tag>*_
+┣ ඬ⃟ 💵 _.transfer *<tipo> <cantidad> <@tag>*_
+┣ ඬ⃟ 💵 _.unreg *<numero de serie>*_
+┗━━━━━━━━━━━━━━━━┛
+ *مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< قسم صناعة الملصقات بـ انواعها 👇🏼 />*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 👽 _.sticker *<responder a imagen o video>*_
+┣ ඬ⃟ 👽 _.sticker *<enlace / link / url>*_
+┣ ඬ⃟ 👽 _.sticker2 *<responder a imagen o video>*_
+┣ ඬ⃟ 👽 _.sticker2 *<enlace / link / url>*_
+┣ ඬ⃟ 👽 _.s *<responder a imagen o video>*_
+┣ ඬ⃟ 👽 _.s *<enlace / link / url>*_
+┣ ඬ⃟ 👽 _.s2 *<responder a imagen o video>*_
+┣ ඬ⃟ 👽 _.s2 *<enlace / link / url>*_
+┣ ඬ⃟ 👽 _.emojimix *<emoji 1>&<emoji 2>*_
+┣ ඬ⃟ 👽 _.scircle *<imagen>*_
+┣ ඬ⃟ 👽 _.sremovebg *<imagen>*_
+┣ ඬ⃟ 👽 _.semoji *<tipo> <emoji>*_
+┣ ඬ⃟ 👽 _.attp *<texto>*_
+┣ ඬ⃟ 👽 _.attp2 *<texto>*_
+┣ ඬ⃟ 👽 _.attp3 *<texto>*_
+┣ ඬ⃟ 👽 _.ttp *<texto>*_
+┣ ඬ⃟ 👽 _.ttp2 *<texto>*_
+┣ ඬ⃟ 👽 _.ttp3 *<texto>*_
+┣ ඬ⃟ 👽 _.ttp4 *<texto>*_
+┣ ඬ⃟ 👽 _.ttp5 *<texto>*_
+┣ ඬ⃟ 👽 _.pat *<@tag>*_
+┣ ඬ⃟ 👽 _.slap *<@tag>*_
+┣ ඬ⃟ 👽 _.kiss *<@tag>*_
+┣ ඬ⃟ 👽 _.dado_
+┣ ඬ⃟ 👽 _.wm *<packname> <author>*_
+┣ ඬ⃟ 👽 _.stickermarker *<efecto> <imagen>*_
+┣ ඬ⃟ 👽 _.stickerfilter *<efecto> <imagen>*_
+┗━━━━━━━━━━━━━━━━┛
+ *مطور من قبل القياده الخال شخصياً🦅🇾🇪*
+┏━━━━━━━━━━━━━━━━┓
+┃ *< اوامر الفيروسات💀☠️ />*
+┃≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡┃
+┣ ඬ⃟ 💀 _.crash𝟷_
+┣ ඬ⃟ 💀 _.crash2_
+┣ ඬ⃟ 💀 _.crash3_
+┣ ඬ⃟ 💀 _.crash4_
+┣ ඬ⃟ 💀 _.crash5_
+┣ ඬ⃟ 💀 _.crash6_
+┣ ඬ⃟ 💀 _.crash7_
+┣ ඬ⃟ 💀 _.crash8_
+┗━━━━━━━━━━━━━━━━┛
+*لعرض باقي الاوامر مما يقارب (1000) الف امر يمكنك تنزيلهم من قناة تحديثات البوت تليجرام عبر الرابط المباشر الى تحميل ملف الاوامر كامل 👇🏼*
+t.me/Driving_uncle_personally/5205
+t.me/Driving_uncle_personally/5205
+┗━━━━━━━━━━━━━━━━┛
+`.trim()
+let buttons = [
+{ buttonId: '#donar', buttonText: { displayText: '📮 المالك 📮' }, type: 1 },
+//{ buttonId: '#terminosycondiciones', buttonText: { displayText: '📋 𝚃𝙴𝚁𝙼𝙸𝙽𝙾𝚂 𝚈 𝙲𝙾𝙽𝙳𝙸𝙲𝙸𝙾𝙽𝙴𝚂 📋' }, type: 1 }]
+{ buttonId: '#infobot', buttonText: { displayText: '🐾 المطور 🐾' }, type: 1 }]
+let buttonMessage = {
+image: pp,
+caption: str.trim(),
+mentions: [m.sender],
+footer: `*${wm}*`,
+buttons: buttons,
+headerType: 4,
+contextInfo: {
+mentionedJid: [m.sender],
+externalAdReply: {
+showAdAttribution: true,
+mediaType: 'VIDEO',
+mediaUrl: null,
+title: '👑 بوت القياده الخال 👑',
+body: null,
+thumbnail: img,
+sourceUrl: `https://www.paypal.me/TheShadowBrokers133`
+}}}
+conn.sendMessage(m.chat, buttonMessage, { quoted: m })
+//await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
+} catch {
+conn.reply(m.chat, '*[❗خطاء❗] 𝙴𝙻 𝙼𝙴𝙽𝚄 𝚃𝙸𝙴𝙽𝙴 𝚄𝙽 𝙴𝚁𝚁𝙾𝚁 𝚈 𝙽𝙾 𝙵𝚄𝙴 𝙿𝙾𝚂𝙸𝙱𝙻𝙴 𝙴𝙽𝚅𝙸𝙰𝚁𝙻𝙾, 𝚁𝙴𝙿𝙾𝚁𝚃𝙴𝙻𝙾 𝙰𝙻 𝙿𝚁𝙾𝙿𝙸𝙴𝚃𝙰𝚁𝙸𝙾 𝙳𝙴𝙻 𝙱𝙾𝚃*', m)
+}}
+handler.command = /^(menu|menú|memu|memú|help|info|comandos|allmenu|2help|menu1.2|ayuda|commands|commandos|cmd)$/i
+handler.exp = 50
+handler.fail = null
+export default handler
+function clockString(ms) {
+let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
